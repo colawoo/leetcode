2da4dda4592d544dfe2c517a80842a2f8b377157
colawoo <xu0822@gmail.com>
2020-03-15 01:56:51 +0800

    java code improvement for problem 1:Two Sum

--- algorithms/java/src/twoSum/TestTwoSum.java
+++ algorithms/java/src/twoSum/TestTwoSum.java
@@ -8,7 +8,7 @@ public class TestTwoSum {
     @Test
     public void testTwoSum() {
         TwoSum twoSum = new TwoSum();
-        int[] sum = twoSum.twoSum(new int[]{2, 7, 11, 15}, 9);
+        int[] sum = twoSum.twoSumV2(new int[]{2, 7, 11, 15}, 9);
         Assert.assertEquals(0, sum[0]);
         Assert.assertEquals(1, sum[1]);
     }
--- algorithms/java/src/twoSum/TwoSum.java
+++ algorithms/java/src/twoSum/TwoSum.java
@@ -1,7 +1,29 @@
 package twoSum;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example:
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
 public class TwoSum {
 
+    /**
+     *
+     * Time complexity : O(n^2)
+     *
+     * Space complexity : O(1)
+     *
+     */
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
@@ -10,6 +32,50 @@ public class TwoSum {
                 }
             }
         }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+    /**
+     *
+     * Time complexity : O(n)
+     *
+     * Space complexity : O(n)
+     *
+     */
+    public int[] twoSumV1(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int ele = target - nums[i];
+            if (map.containsKey(ele) && map.get(ele) != i) {
+                return new int[]{i, map.get(ele)};
+            }
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+    /**
+     *
+     * Time complexity : O(n)
+     *
+     * Space complexity : O(n)
+     *
+     */
+    public int[] twoSumV2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+        }
+
         throw new IllegalArgumentException("No two sum solution");
     }
 
