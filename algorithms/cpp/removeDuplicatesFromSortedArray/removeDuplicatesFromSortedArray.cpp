5485b811f00f4b23de5598f3b8368dfaa267d3ac
colawoo <xu0822@gmail.com>
2020-03-23 05:45:47 +0800

    C++ solution for problem 26. Remove Duplicates from Sorted Array

---
+++ algorithms/cpp/removeDuplicatesFromSortedArray/removeDuplicatesFromSortedArray.cpp
@@ -0,0 +1,52 @@
+// 26. Remove Duplicates from Sorted Array (https://leetcode.com/problems/remove-duplicates-from-sorted-array/)
+//
+// Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+// Example 1:
+
+// Given nums = [1,1,2],
+
+// Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+// It doesn't matter what you leave beyond the returned length.
+// Example 2:
+
+// Given nums = [0,0,1,1,1,2,2,3,3,4],
+
+// Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+
+// It doesn't matter what values are set beyond the returned length.
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+    if (nums.size() == 0) return 0; 
+    int len=0;
+    for (int i=1; i<nums.size(); i++) {
+        if (nums[len] != nums[i]) {
+            nums[++len] = nums[i];
+        }
+    }
+    return len+1;
+}
+
+int main() {
+
+    int nums[] = {};
+    vector<int> v(nums, nums + sizeof(nums)/sizeof(int));
+
+    int length = removeDuplicates(v);
+    cout << "the new length is " << length << endl;
+    for (int i=0; i<length; i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+}
+
+
