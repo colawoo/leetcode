3444fc40826eaaff4014671e9d563b6f2a0055a7
colawoo <xu0822@gmail.com>
2024-02-22 15:39:51 +0800

    refactor the Java solution for problem 1. Two Sum

--- algorithms/java/src/solution/TwoSum.java
+++ algorithms/java/src/solution/TwoSum.java
@@ -4,79 +4,85 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
  *
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
  *
- * Example:
+ * You can return the answer in any order.
  *
- * Given nums = [2, 7, 11, 15], target = 9,
  *
- * Because nums[0] + nums[1] = 2 + 7 = 9,
- * return [0, 1].
+ * Example 1:
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ *
+ * Example 2:
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ *
+ * Example 3:
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ *
+ * Constraints:
+ * 2 <= nums.length <= 10^4
+ * -10^9 <= nums[i] <= 10^9
+ * -10^9 <= target <= 10^9
+ * Only one valid answer exists.
  */
 public class TwoSum {
 
     /**
-     *
+     * Solution 1: Brute Force
      * Time complexity : O(n^2)
-     *
      * Space complexity : O(1)
-     *
      */
-    public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
+    public int[] solution1(int[] nums, int target) {
+        int len = nums.length;
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
                 if (nums[i] == target - nums[j]) {
-                    return new int[] {i, j};
+                    return new int[]{i, j};
                 }
             }
         }
-        throw new IllegalArgumentException("No two sum solution");
+        return new int[]{};
     }
-
 
     /**
-     *
+     * Solution 2: Two-pass Hash Table
      * Time complexity : O(n)
-     *
      * Space complexity : O(n)
-     *
      */
-    public int[] twoSumV1(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
+    public int[] solution2(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            numMap.put(nums[i], i);
         }
-
-        for (int i = 0; i < nums.length; i++) {
-            int ele = target - nums[i];
-            if (map.containsKey(ele) && map.get(ele) != i) {
-                return new int[]{i, map.get(ele)};
+        for (int i = 0; i < len; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement) && numMap.get(complement) != i) {
+                return new int[]{i, numMap.get(complement)};
             }
         }
-
-        throw new IllegalArgumentException("No two sum solution");
+        return new int[]{};
     }
-
 
     /**
-     *
+     * Solution 3: One-pass Hash Table
      * Time complexity : O(n)
-     *
      * Space complexity : O(n)
-     *
      */
-    public int[] twoSumV2(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
+    public int[] solution3(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
-            if (map.containsKey(target - nums[i])) {
-                return new int[]{map.get(target - nums[i]), i};
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
             }
+            numMap.put(nums[i], i);
         }
-
-        throw new IllegalArgumentException("No two sum solution");
+        return new int[]{};
     }
-
 }
--- algorithms/java/src/test/TwoSumTest.java
+++ algorithms/java/src/test/TwoSumTest.java
@@ -7,11 +7,29 @@ import solution.TwoSum;
 public class TwoSumTest {
 
     @Test
-    public void testTwoSum() {
+    public void testSolution1() {
         TwoSum twoSum = new TwoSum();
-        int[] sum = twoSum.twoSumV2(new int[]{2, 7, 11, 15}, 9);
-        Assert.assertEquals(0, sum[0]);
-        Assert.assertEquals(1, sum[1]);
+        int[] result1 = twoSum.solution1(new int[]{2, 7, 11, 15}, 9);
+        Assert.assertEquals(2, result1.length);
+        Assert.assertEquals(0, result1[0]);
+        Assert.assertEquals(1, result1[1]);
     }
 
+    @Test
+    public void testSolution2() {
+        TwoSum twoSum = new TwoSum();
+        int[] result2 = twoSum.solution2(new int[]{3, 2, 4}, 6);
+        Assert.assertEquals(2, result2.length);
+        Assert.assertEquals(0, result2[0]);
+        Assert.assertEquals(1, result2[1]);
+    }
+
+    @Test
+    public void testSolution3() {
+        TwoSum twoSum = new TwoSum();
+        int[] result3 = twoSum.solution3(new int[]{3, 3}, 6);
+        Assert.assertEquals(2, result3.length);
+        Assert.assertEquals(1, result3[0]);
+        Assert.assertEquals(2, result3[1]);
+    }
 }
