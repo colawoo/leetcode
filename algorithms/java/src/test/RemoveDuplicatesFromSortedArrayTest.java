70c2a0ac19c7aac8cb6202e6fe817f1c37fb46ad
colawoo <xu0822@gmail.com>
2024-02-27 17:55:55 +0800

    refactor the Java solution for problem 26.Remove Duplicates From Sorted Array

--- algorithms/java/src/test/RemoveDuplicatesFromSortedArrayTest.java
+++ algorithms/java/src/test/RemoveDuplicatesFromSortedArrayTest.java
@@ -7,30 +7,25 @@ import solution.RemoveDuplicatesFromSortedArray;
 public class RemoveDuplicatesFromSortedArrayTest {
 
     @Test
-    public void testRemoveDuplicatesFromSortedArray() {
-
+    public void testRemoveDuplicates1() {
         int[] nums = {1, 1, 2};
         RemoveDuplicatesFromSortedArray r = new RemoveDuplicatesFromSortedArray();
-        int len = r.removeDuplicatesFromSortedArray(nums);
+        int len = r.removeDuplicates(nums);
         Assert.assertEquals(2, len);
         Assert.assertEquals(1, nums[0]);
         Assert.assertEquals(2, nums[1]);
-
     }
 
     @Test
-    public void testRemoveDuplicatesFromSortedArray1() {
-
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+    public void testRemoveDuplicates2() {
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
         RemoveDuplicatesFromSortedArray r = new RemoveDuplicatesFromSortedArray();
-        int len = r.removeDuplicatesFromSortedArray(nums);
+        int len = r.removeDuplicates(nums);
         Assert.assertEquals(5, len);
         Assert.assertEquals(0, nums[0]);
         Assert.assertEquals(1, nums[1]);
         Assert.assertEquals(2, nums[2]);
         Assert.assertEquals(3, nums[3]);
         Assert.assertEquals(4, nums[4]);
-
     }
-
 }
